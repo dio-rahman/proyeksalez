53e64f9b2847d6572c58e40e8ab625da6e882227
dio-rahman <dio.rahman.tif23@polban.ac.id>
2025-04-08 08:38:05 +0700

    Delete file lama (app/src/main/java/com/salez/kasir directory)

--- app/src/main/java/com/salez/kasir/ui/theme/Color.kt
+++
@@ -1,7 +0,0 @@
-package com.salez.kasir.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val SecondaryColor = Color(0xFFFF6666)
-val PrimaryVariant = Color(0xFF212121)
-val PrimaryColor = Color(0xFFB71C1C)
--- app/src/main/java/com/salez/kasir/ui/theme/Type.kt
+++
@@ -1,5 +0,0 @@
-package com.salez.kasir.ui.theme
-
-import androidx.compose.material3.Typography
-
-val Typography = Typography()
